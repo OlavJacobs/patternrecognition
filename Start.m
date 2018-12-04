54e17b3359c84890487878188e1a1cfaca612efc
LiamBosland <45391133+LiamBosland@users.noreply.github.com>
2018-12-04 11:29:58 +0100

    Het begin

---
+++ Start.m
@@ -0,0 +1,11 @@
+clc
+clear all
+close all
+prwaitbar OFF
+a = prnist([0:4],[1:40:40]);
+a = im_resize(a,[40 40]);
+b = +prdataset(a);
+featset = im_features(a,'all')
+labels = getlab(featset')
+featset = +featset
+show(a)
