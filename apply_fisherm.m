241b158824513aa012643a87dd037b9cd54b640e
SimonStouten <45373972+SimonStouten@users.noreply.github.com>
2018-12-13 16:55:07 +0100

    Delete apply_fisherm.m

--- apply_fisherm.m
+++
@@ -1,8 +0,0 @@
-function [x_fisher,w_fisher] = apply_fisherm(x,N)
-% This function implements a Fisher Mapping to transform input data.
-% The input to this function should be a real-valued matrix x
-% No pre-whitening variance preservation is used, thus ALF = []
-w_fisher = fisherm(x,N);
-x_fisher = x*w_fisher;
-end
-
