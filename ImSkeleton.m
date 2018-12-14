a02107e1860ffd369d46736eec5be56f3d97aeab
SimonStouten <45373972+SimonStouten@users.noreply.github.com>
2018-12-14 14:54:00 +0100

    Created ImSkeleton
    Representing images in morphological skeleton-form --> Reduced structure

---
+++ ImSkeleton.m
@@ -0,0 +1,15 @@
+clc;
+clear all;
+close all;
+prwaitbar off;
+
+N = 10;
+a = prnist(0:9,1:N);
+a = im_resize(a,[20 20]);
+
+figure;
+show(a)
+
+a_skel = im_skel(prdataset(a));
+figure;
+show(a_skel);
