4f2ef7b688a30d7d3e450aed85ec46c4dd68db64
Olav Jacobs <38039136+OlavJacobs@users.noreply.github.com>
2019-01-11 09:48:15 +0100

    Worked on the kernel pca method

--- apply_kernel_pca.m
+++ apply_kernel_pca.m
@@ -1,11 +1,20 @@
 function [a,W] = apply_kernel_pca(a,N)
+% This paper contains an example for kernel mapping with dissimilarity for
+% the NIST dataset
+% http://rduin.nl/papers/pr_06_protosel.pdf
 
 scale_map = scalem(a,'variance');
 a = a*scale_map;
 
-W = setbatch(kernelm(a,proxm('d',3)));          % proxm('d',1)
+m = knnm(1);
 
-W = W(:,1:N);
+W = kernelm(a,m,'random',N);
+
+%W = setbatch(kernelm(a,proxm('d',3)));          % proxm('d',1)
+% setbatch allows for batch calculation, this is nessesary because
+% otherwise matlab wil run out of memory (check later if it can be removed)
+
+%W = W(:,1:N);
 
 a = a*W;
 
