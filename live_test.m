b1d464f140a6b13b57e67b36e96011cf5360f33e
Olav Jacobs <38039136+OlavJacobs@users.noreply.github.com>
2019-01-16 17:31:29 +0100

    Update live_test.m

--- live_test.m
+++ live_test.m
@@ -1,11 +1,21 @@
 %% This scirpt is for test our own written numbers using our classifier
+
+W = combc;
+
 data = [];
+labels = [];
 
 for i = 0:9
     for j = 1:10
         s = strcat('HandwrittenDigits/',num2str(i),'_',num2str(j),'.png');
         image = im2bw(rgb2gray(imread(s)),0.1);
-        imdata = im_resize(im2obj(image),[20 20])
+        imdata = im_resize(im2obj(image),[20 20]);
         data = [data; imdata];
+        labels = [labels; strcat('digit_',num2str(i))];
     end
 end
+
+data = setlabels(data,labels);
+%%
+
+[E,C] = testc(data,W);
