0be6702451d278ff409a0b1a64ee6425025d782d
Olav Jacobs <38039136+OlavJacobs@users.noreply.github.com>
2019-01-25 13:56:05 +0100

    Added case 2 + a test for different combiners

--- dissim_class_test_case2.m
+++ dissim_class_test_case2.m
@@ -12,31 +12,31 @@ close all
 prwaitbar OFF
 prwarning OFF
 
-N = 1000;           % The number of objects taken from the dataset per class
+N = 100;           % The number of objects taken from the dataset per class
 
 a = prnist(0:9,1:N);
 a = im_resize(a,[20 20]);
 
-% prtime(300) % Increase the maximum time for optimisation
+% prtime(60) % Increase the maximum time for optimisation
 
 %% Split the dataset in training en test data
 n = length(classnames(a));             % The number of classes taken into consideration (default: 10 for classes 0 - 9)
-f = 0.8;
 
-seltrain = repmat({1:N*f},1,n); % Building cell vector of entries [1 : N*f(k)]
-seltest = repmat({N*f+1:N},1,n);% Building cell vector of entries [N*f(k)+1 : end]
+E_NIST = 0;         % Initalize the error sum
+
+for i = 0:9
+
+seltrain = repmat({1+10*i:10+10*i},1,n); % Building cell vector of entries [1 : N*f(k)]
+seltest = repmat({1:N},1,n);% Building cell vector of entries [N*f(k)+1 : end]
 trainset = prdataset(seldat(a,[],[],seltrain)); % Selecting training objects from original dataset
 testset = prdataset(seldat(a,[],[],seltest));   % Selecting test objects from original dataset
 
 %% Make the classifiers
 
-[W,E,combc,E2] = make_fdsc(trainset,testset,50,10,4); % Build dissimilarity space classifier, see make_fdsc
+W = make_fdsc(trainset,testset,10,1,1); % Build dissimilarity space classifier, see make_fdsc
 
-[E_min,Id] = min(E); % Find the smallest error of the classifiers
-W_opt = W{Id};       % Classifier corrisponding to the smallest error
+E_NIST = E_NIST + nist_eval('my_rep',W{1}); % Sum all the errors calculated with nist eval
 
-E_Bestc = nist_eval('my_rep',W_opt); % Re-evauate the best found classifier, this time with nist_eval
-E_comb = nist_eval('my_rep',combc);  % Test the combined classifier
+end
 
-
-
+E_NIST = E_NIST/10;
